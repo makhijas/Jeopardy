aab41d53702d7ec006c475ef5e3f85a5df4eff4c
Swati Makhija <swatimakhija@Swatis-MacBook-Air.local>
2021-01-27 14:52:40 -0800

    added alert popup with input for answers; added logic for correct vs incorrect answer and made object for questions

--- app.js
+++ app.js
@@ -8,7 +8,31 @@
 //     document.getElementById("demo").innerHTML = "Hello World";
 // });
 
-var buttonEl = document.getElementsByClassName('button');
-if(buttonEl){
-  buttonEl.addEventListener('click', alert("hi"));
+// const buttonEl = document.getElementsByClassName("button");
+// if(buttonEl){
+//   document.getElementsByClassName("button").addEventListener("click", alert("hi"));
+// }
+
+// let button = document.getElementById("button")
+// const music100 = () => {
+//     alert("hi");
+//   }
+
+const music = {
+    100: "Q1", 
+    200: "Q2",
+    300: "Q3",
+    400: "Q4",
+    500: "Q5"
+}
+const musicQs = () => {
+    let x = ""
+    let clue=prompt("Beethoven, Vivaldi, Tchaikovsky and Mozart are from this era");
+    if (clue=== "Classical"){
+       x="Correct!";
+      alert(x);
+   } else {
+       x="Incorrect!";
+       alert(x);
+}
 }
