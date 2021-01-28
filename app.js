89de8f0f52ec309f1b4398f9838ebd3ee687e032
Swati Makhija <swatimakhija@Swatis-MacBook-Air.local>
2021-01-28 11:43:32 -0800

    added scoreUp and scoreDown functions

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-// let button = document.getElementsByClassName("button")
+//Array of dollar amounts
+dollar = [100, 200, 300, 400, 500]
 
-// button.addEventListener ("click", function() {
-//     alert("did something");
-//   });
-  
-// document.getElementById("myBtn").addEventListener("click", function() {
-//     document.getElementById("demo").innerHTML = "Hello World";
-// });
-
-// const buttonEl = document.getElementsByClassName("button");
-// if(buttonEl){
-//   document.getElementsByClassName("button").addEventListener("click", alert("hi"));
-// }
-
-// let button = document.getElementById("button")
-// const music100 = () => {
-//     alert("hi");
-//   }
-
-const music = {
+//Question and Answer objects
+const musicQ = {
     100: "Beethoven, Vivaldi, Tchaikovsky and Mozart are from this era", 
     200: "Q2",
     300: "Q3",
@@ -33,40 +17,59 @@ const musicA = {
     400: "Q4",
     500: "Q5"
 }
+
+
+// const music = () => {
+//     if(document.getElementById('button').clicked = true) {
+//         points = (document.getElementById("button").innerHTML).split("$")
+//         console.log(points)
+//         let clue = prompt(musicQ[points[1]])
+
+//         if(clue === musicA[100]) {
+//             alert("Correct!")
+//             scoreUp()
+//         } else {
+//             alert("Incorrect")
+//             scoreDown()
+//         }
+//     }
+// }
+
 const music100 = () => {
-    let xMusic100 = ""
-    let clueMusic100 = prompt(music[100]);
+    let clueMusic100 = prompt(musicQ[100]);
     if (clueMusic100 === musicA[100]){
-        xMusic100="Correct!"
-        alert(xMusic100)
-        score()
+        alert("Correct!")
+        scoreUp()
     } else {
-        x="Incorrect!";
-        alert(x);
+        alert("Incorrect!")
+        scoreDown()
     }
 }
 
-const music200 = () => {
-    let xMusic200 = ""
-    let clueMusic200 = prompt(music[200]);
-    if (clueMusic200 === musicA[200]){
-    xMusic200="Correct!";
-    alert(xMusic200);
-    } else {
-        x="Incorrect!";
-        alert(x);
-    }
-}
+// }
+// const music200 = () => {
+//     let xMusic200 = ""
+//     let clueMusic200 = prompt(musicQ[200]);
+//     if (clueMusic200 === musicA[200]){
+//     xMusic200="Correct!";
+//     alert(xMusic200);
+//     } else {
+//         x="Incorrect!";
+//         alert(x);
+//     }
+// }
 
-// let scoreSum = parseFloat(document.getElementById("scoreDisplay").innerHTML)
-// console.log(scoreSum)
-// a = document.getElementById("button").innerHTML.split("$")
-// console.log(a)
-// //console.log(a[1])
 
-const score = () => {
+const scoreUp = () => {
         let currentScore = parseFloat(document.getElementById("scoreDisplay").innerHTML)
         buttonMoney = document.getElementById("button").innerHTML.split("$")
         currentScore = parseFloat(buttonMoney[1]) + currentScore
         document.getElementById("scoreDisplay").innerHTML = currentScore;
+}
+
+const scoreDown = () => {
+    let currentScore = parseFloat(document.getElementById("scoreDisplay").innerHTML)
+    buttonMoney = document.getElementById("button").innerHTML.split("$")
+    currentScore = currentScore -  parseFloat(buttonMoney[1])
+    document.getElementById("scoreDisplay").innerHTML = currentScore;
 }
